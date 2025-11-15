07c518bb0cda165e3c009bc700e530664cf168e0
Phuc Tran <tqp1610@gmail.com>
2025-11-15 17:37:23 +0700

    feat: Add animated typing text component with customizable properties
    feat: Implement video text component with SVG masking and customizable text effects
    
    feat: Create wave background component using WebGL for dynamic visuals
    
    feat: Develop WoofyHoverImage component with advanced shader effects and hover animations

---
+++ Animated-portfolio/src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+import { AnimatePresence, motion } from 'framer-motion'
+
+const Header = () => {
+    return (
+        <AnimatePresence>
+            <motion.header>
+                <div className=''>
+
+                </div>
+            </motion.header>
+        </AnimatePresence>
+    )
+}
+
+export default Header
