a81329c660775ef7f2c178b57402e2123c08f687
Phuc Tran <tqp1610@gmail.com>
2025-11-15 18:15:12 +0700

    feat: add lucide-react dependency and update Header component with icons and animations

--- Animated-portfolio/src/components/Header/Header.tsx
+++ Animated-portfolio/src/components/Header/Header.tsx
@@ -1,16 +1,34 @@
 import { AnimatePresence, motion } from 'framer-motion'
+import { BorderBeam } from '../lightswind/border-beam'
+import { BookCheckIcon } from 'lucide-react'
 
 const Header = () => {
+
+    const handleScrollTo = (id: string) => { 
+        const element = document.querySelector(id)
+    }
+
     return (
         <AnimatePresence>
-            <motion.header>
+            <motion.header
+                initial={{ y: -100, top: 20, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                exit={{ y: -100, opacity: 0, transition: { duration: 0.4 } }}
+                transition={{ duration: 0.2, ease: "easeOut" }}
+                className="fixed top-0 left-0 right-0 z-50 flex justify-center px-4">
                 <div
                     className="border border-gray-100 dark:border-gray-900 backdrop-blur-xl
             w-full xl:max-w-6xl rounded-full
             flex items-center justify-between px-6 py-3
             transition-all duration-300"
                 >
-                    <h1 className="text-2xl font-bold">Animated Portfolio</h1>
+                    <BorderBeam />
+                    <a
+                        onClick={() => handleScrollTo("#hero")}
+                        className="cursor-pointer font-bold text-lg text-gray-800 dark:text-white"
+                    >
+                        <BookCheckIcon />
+                    </a>
                 </div>
             </motion.header>
         </AnimatePresence>
