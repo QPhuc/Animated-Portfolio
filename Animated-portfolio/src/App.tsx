55636a8747cd9dc103b57dbd5c3d23546023eb71
Phuc Tran <tqp1610@gmail.com>
2025-11-15 18:12:31 +0700

    chore: update dependencies and configure Tailwind CSS
    - Added @tailwindcss/postcss and autoprefixer to devDependencies.
    - Updated App component to include text color.
    - Enhanced Header component with a styled div and header text.
    - Replaced Tailwind CSS import in index.css with @tailwind directives.
    - Removed Tailwind CSS from Vite plugins and added PostCSS configuration.
    - Created Tailwind CSS configuration file for content and theme settings.

--- Animated-portfolio/src/App.tsx
+++ Animated-portfolio/src/App.tsx
@@ -4,7 +4,7 @@ import Header from './components/Header/Header'
 function App() {
 
   return (
-    <div className="bg-background min-h-screen flex items-center justify-center">
+    <div className="bg-background min-h-screen flex items-center justify-center text-color-red">
       <Header />
     </div>
   )
--- Animated-portfolio/src/components/Header/Header.tsx
+++ Animated-portfolio/src/components/Header/Header.tsx
@@ -4,8 +4,13 @@ const Header = () => {
     return (
         <AnimatePresence>
             <motion.header>
-                <div className=''>
-
+                <div
+                    className="border border-gray-100 dark:border-gray-900 backdrop-blur-xl
+            w-full xl:max-w-6xl rounded-full
+            flex items-center justify-between px-6 py-3
+            transition-all duration-300"
+                >
+                    <h1 className="text-2xl font-bold">Animated Portfolio</h1>
                 </div>
             </motion.header>
         </AnimatePresence>
