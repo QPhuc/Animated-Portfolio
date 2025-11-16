754d580436e5092324174abcd6ae3352e4b5b407
Phuc Tran <tqp1610@gmail.com>
2025-11-16 18:32:53 +0700

    Fix tailwind

--- Animated-portfolio/src/App.tsx
+++ Animated-portfolio/src/App.tsx
@@ -1,11 +1,29 @@
+import ReactLenis from 'lenis/react'
 import './App.css'
 import Header from './components/Header/Header'
+import StripesBackground from './components/lightswind/stripes-background'
+import HeroSection from './components/HeroSection/HeroSection'
 
 function App() {
 
   return (
-    <div className="bg-background min-h-screen flex items-center justify-center text-color-red">
-      <Header />
+    <div className="bg-background min-h-screen flex items-center justify-center">
+      <StripesBackground className={"fixed z-0 blur-xs"} />
+      <ReactLenis root>
+        <Header />
+        <div
+          className="w-full bg-transparent max-w-5xl px-4 my-30
+          flex items-center justify-center 
+          lg:rounded-3xl backdrop-blur-xl border-gray-100 dark:border-gray-900"
+        >
+          <div className="z-10">
+            {/* Give IDs to sections */}
+            <div id="home">
+              <HeroSection />
+            </div>
+          </div>
+        </div>
+      </ReactLenis>
     </div>
   )
 }
